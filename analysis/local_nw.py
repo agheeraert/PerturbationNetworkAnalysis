3ee9a2a0363a18d3007b5371fed329a3adf46fa7
Aria GHEEREART <aghee@lama-e61.univ-savoie.fr>
2019-09-26 14:52:11 +0200

    The local nw was false all along, lets try again

--- analysis/local_nw.py
+++ analysis/local_nw.py
@@ -10,43 +10,34 @@ import pickle as pkl
 import pandas as pd
 from Bio.PDB.Polypeptide import aa3
 
-BASE_FOLDER = '/home/agheerae/results/avg_sims/pertnet/'
+BASE_FOLDER = '/home/agheerae/results/avg_sims/nets/'
+APO_FOLDER = jn(BASE_FOLDER, 'apo')
+PRFAR_FOLDER = jn(BASE_FOLDER, 'prfar')
 OUT_FOLDER = '/home/agheerae/results/local_nw/noH/'
-floaters = True
 q = len(aa3)
 aa_to_id = dict(zip(aa3, range(q)))
 
-def nw(net, node):
-    return net.degree(node)/net.degree(node, weight='weight')
+def delta_nw(net2, net1, node):
+    return net2.degree(node)/net2.degree(node, weight='weight')-net1.degree(node)/net1.degree(node, weight='weight')
 
 if __name__ =='__main__':
     W, K, WK = np.zeros((7, 6)), np.zeros((7, 6)), np.zeros((7, 6))
     for cutoff in range(3, 10):
-        folder = jn(BASE_FOLDER, 'cutoff_'+str(cutoff))
-        if cutoff == 3 and floaters: #FLAG3
-            L_threshs = sorted([float(file.split('_')[1].split('.')[0].replace('-','.')) for file in listdir(folder) if file[-1]=='p'])
-        else:
-            L_threshs = sorted([int(file.split('_')[1].split('.')[0]) for file in listdir(folder) if file[-1]=='p'])
- 
-        for thresh in L_threshs: 
-            if cutoff == 3: #FLAG3
-                thresh_str = str(thresh).replace('.', '-')
-                if thresh == 1.0:
-                    thresh_str = '1'
-                elif thresh == 0.0:
-                    thresh_str = '0'  
-                thresh = int(thresh*10)
+        apo_net = nx.read_gpickle(jn(apo_folder, str(cutoff)+'.p'))
+        prfar_net = nx.read_gpickle(jn(prfar_folder, str(cutoff)+'.p'))
+        new_net = nx.Graph()
+        for u, v in nx.compose(apo_net, prfar_net).edges():
+            if (u,v) in apo_net and (u, v) in prfar_net:
+                new_weight=delta_nw(prfar_net, apo_net, u)+nw(prfar_net, apo_net, net, v)
+            elif (u,v) in apo_net and (u,v) not in prfar_net:
+                new_weight = delta_nw(apo_net)
             else:
-                thresh_str = str(thresh)
-            net = nx.read_gpickle(jn(folder, str(cutoff)+'_'+thresh_str+'.p'))
-            new_net = nx.Graph()
-            for u, v in net.edges():
-                new_weight=abs(nw(net, u)+nw(net, v))
-                if nw(net, u)+nw(net, u)>0:
-                    new_net.add_edge(u, v, color='red', weight=new_weight)
-                elif nw(net, u)+nw(net, v)<0:
-                    new_net.add_edge(u, v, color='blue', weight=new_weight)
-            nx.write_gpickle(new_net, jn(OUT_FOLDER, str(cutoff)+'_'+thresh_str+'.p'))
+                new_weight = delta_nw(prfar_net)
+            if new_weight > 0:
+                new_net.add_edge(u, v, color='red', weight=abs(new_weight))
+            elif new_weight < 0:
+                new_net.add_edge(u, v, color='blue', weight=abs(new_weight))
+        nx.write_gpickle(new_net, jn(OUT_FOLDER, str(cutoff)+'.p'))
 
 
 
