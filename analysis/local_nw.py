1c50ee194ab7ffdd373a6d6371ecb142e1a5026e
Aria GHEERAERT <aria.gheeraert@gmail.com>
2019-09-30 13:16:56 +0200

    debugged local nw

--- analysis/local_nw.py
+++ analysis/local_nw.py
@@ -25,22 +25,22 @@ def delta_nw(net2, net1, node):
     return nw(net2, node)-nw(net1,node)
 
 if __name__ =='__main__':
-    W, K, WK = np.zeros((7, 6)), np.zeros((7, 6)), np.zeros((7, 6))
     for cutoff in range(3, 10):
         apo_net = nx.read_gpickle(jn(apo_folder, str(cutoff)+'.p'))
         prfar_net = nx.read_gpickle(jn(prfar_folder, str(cutoff)+'.p'))
         new_net = nx.Graph()
         for u, v in nx.compose(apo_net, prfar_net).edges():
-            if (u,v) in apo_net and (u, v) in prfar_net:
-                new_weight=delta_nw(prfar_net, apo_net, u)+nw(prfar_net, apo_net, net, v)
-            elif (u,v) in apo_net and (u,v) not in prfar_net:
-                new_weight = nw(apo_net, u)+nw(apo_net,v)
+            if (u,v) in apo_net.edges() and (u, v) in prfar_net.edges():
+                new_weight = delta_nw(prfar_net, apo_net, u)
+            elif (u,v) in apo_net.edges() and (u,v) not in prfar_net.edges():
+                new_weight = -nw(apo_net, u)-nw(apo_net,v)
             else:
                 new_weight = nw(prfar_net, u)+nw(prfar_net,v)
+            print(new_weight)
             if new_weight > 0:
-                new_net.add_edge(u, v, color='red', weight=abs(new_weight))
+                new_net.add_edge(u, v, color='r', weight=abs(new_weight))
             elif new_weight < 0:
-                new_net.add_edge(u, v, color='blue', weight=abs(new_weight))
+                new_net.add_edge(u, v, color='g', weight=abs(new_weight))
         nx.write_gpickle(new_net, jn(OUT_FOLDER, str(cutoff)+'_0.p'))
         empty, thresh = False, 1
         while not empty:
