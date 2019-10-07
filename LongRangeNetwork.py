0f56ae235758f426eeadb36c95723edb08197913
Aria GHEEREART <aghee@lama-e61.univ-savoie.fr>
2019-10-07 16:24:51 +0200

    LRN

---
+++ LongRangeNetwork.py
@@ -0,0 +1,80 @@
+from os import listdir
+from os.path import join as jn
+import networkx as nx
+import matplotlib.pyplot as plt 
+import numpy as np
+import itertools
+import matplotlib.lines as mlines
+import matplotlib
+import pickle as pkl
+import pandas as pd
+from Bio.PDB.Polypeptide import aa3, aa1
+from Bio.PDB import PDBParser
+import warnings
+from tqdm import tqdm
+from Bio.PDB.PDBExceptions import PDBConstructionWarning
+warnings.simplefilter('ignore', PDBConstructionWarning)
+import biographs as bg
+
+
+class CreateLRN():
+    def __init__(self, path1, path2, cutoff=5, lrn_threshold=7):
+        self.cutoff=cutoff
+        self.lrn_threshold = lrn_threshold
+        self.three2one = dict(zip(aa3, aa1))
+        self.one2three = dict(zip(aa1, aa3))
+        self.three2one['5CS'] = 'C'
+        self.three2one['HIP'] = 'H'
+        self.three2one['HID'] = 'H'
+        self.three2one['HIE'] = 'H'
+        self.three2one['GLH'] = 'E'
+        self.three2one['ASH'] = 'D'
+        self.net1 = self.create(path1)
+        self.net2 = self.create(path2)
+
+    def remove_close_edges(self, net):
+        """ removes edges that are too close in the network """
+        L_edge_remove = []
+        for u, v in net.edges():
+            if abs(int(u[1:].split(':')[0]) - int(v[1:].split(':')[0])) <= self.lrn_threshold or u[-1] != v[-1]:
+                L_edge_remove.append((u,v))
+        net.remove_edges_from(L_edge_remove)
+        net.remove_nodes_from(list(nx.isolates(net)))
+        return net
+
+    def create(self, frame_path):
+        """ creates the Long Range Network of a frame """
+        mol = bg.Pmolecule(frame_path)
+        self.net = mol.network(cutoff=self.cutoff, weight=True)
+        self.structure = PDBParser().get_structure('X', frame_path)[0]
+        residues = []
+        for residue in self.structure.get_residues():
+            residues.append(self.three2one[residue.resname])
+        old_labels = self.net.nodes
+        labels = [a+b[1:]+':'+b[0] for a,b in zip(residues, old_labels)]
+        mapping = dict(zip(old_labels, labels))
+        self.net = nx.relabel_nodes(self.net, mapping)
+        self.net = self.remove_close_edges(self.net)
+        return self.net
+
+    def nw(self, net):
+        """ returns the nw for all the nodes in a net """
+        return {elt[0][0]: elt[0][1]/elt[1][1] for elt in zip(net.degree(weight='weight'), net.degree())}
+
+    def get_nw(self):
+        """ get the nw for all the nodes in the built nets"""
+        return self.nw(self.net1), self.nw(self.net2)
+
+if __name__ == "__main__":
+    apo_folder = "/home/aghee/PDB/Apo_frames/Sim1"
+    prfar_folder= "/home/aghee/PDB/Prfar_frames/Sim1"
+    NW1, NW2 = [], []
+    for frame1, frame2 in tqdm(zip(sorted(listdir(apo_folder)), sorted(listdir(prfar_folder))), total=1000):
+        NWs = CreateLRN(jn(apo_folder, frame1), jn(prfar_folder, frame2)).get_nw()
+        NW1.append(NWs[0])
+        NW2.append(NWs[1])
+    pkl.dump(NW1, open("/home/aghee/results/long_range_evolution/NW_apo.p", 'wb'))
+    pkl.dump(NW2, open("/home/aghee/results/long_range_evolution/NW_prfar.p", 'wb'))
+
+
+
---
+++ analysis/plot_nw_over_sim.py
@@ -0,0 +1,86 @@
+from os import listdir
+from os.path import join as jn
+import networkx as nx
+import matplotlib.pyplot as plt 
+import numpy as np
+import itertools
+import matplotlib.lines as mlines
+import matplotlib
+import pickle as pkl
+import pandas as pd
+from Bio.PDB.Polypeptide import aa3
+from math import sqrt, ceil
+
+NW1 = pkl.load(open("/home/aghee/results/long_range_evolution/NW_apo.p", "rb"))
+NW2 = pkl.load(open("/home/aghee/results/long_range_evolution/NW_prfar.p", "rb"))
+# L_aa = set([aa for elt in NW1 for aa in elt])
+# for node in L_aa:
+#     if node[-1] == "H":
+#         node_str = node[0]+str(int(node[1:-2])-253)+node[-1]
+#     else:
+#         node_str = node
+#     f = plt.figure()
+#     NW1_node, NW2_node = [], []
+#     for i in range(len(NW1)):
+#         if node in NW1[i]:
+#             NW1_node.append(NW1[i][node])
+#         else:
+#             NW1_node.append(0)
+#         if node in NW2[i]:
+#             NW2_node.append(NW2[i][node])
+#         else:
+#             NW2_node.append(0)
+#     plt.plot(list(range(len(NW1))), NW1_node, color = 'b')
+#     plt.plot(list(range(len(NW2))), NW2_node, color = 'r')
+#     lines = []
+#     lines.append(mlines.Line2D([], [], color='b', label='APO'))
+#     lines.append(mlines.Line2D([], [], color='r', label='PRFAR'))
+#     f.legend(handles=lines, fontsize=8, bbox_to_anchor=(0.985, 1), ncol=2, fancybox=True, shadow=True)
+#     plt.title(r"$\bf{" + node_str + "}$")
+#     plt.tight_layout()
+#     plt.savefig('/home/aghee/results/long_range_evolution/'+node_str+'.png')
+#     plt.close()
+
+L_bottom = ["G202:F", "T142:F", "R133:F", "A224:F", "F227:F", "S225:F", "K19:F", "D11:F", "L50:F", "G20:F", "H228:F"]
+L_sb = ["E67:F", "E71:F", "R18:H", "E91:F", "R95:F", "Y136:H", "V248:F", "R249:F", "L250:F", "R22:H", "Q72:F", "R187:H", "D74:F", "N247:F", "W123:H", "I73:F", "I75:F", "K184:H", "M14:H"]
+L_up = ["N12:H", "N15:H", "P49:H", "G50:H", "V51:H", "G52:H", "G9:H", "P10:H", "G11:H"]
+L_unfolding = ["H53:H", "F54:H", "E56:H", "G57:H", "R59:H", "R60:H", "G9:H", "G57:H"]
+L_interface = ["M121:H", "R5:F", "K99:F", "E167:F", "D98:F", "K181:H"]
+
+
+def multiplot(liste, string):
+    n = len(liste)
+    size = ceil(sqrt(n))
+    f = plt.figure()
+    for j, node in enumerate(liste):
+        plt.subplot(size, size, j+1)
+        NW1_node, NW2_node = [], []
+        if node[-1] == "H":
+            node_str = node[0]+str(int(node[1:-2])+253)+node[-2:]
+        else:
+            node_str = node
+        for i in range(len(NW1)):
+            if node_str in NW1[i]:
+                NW1_node.append(NW1[i][node_str])
+            else:
+                NW1_node.append(0)
+            if node_str in NW2[i]:
+                NW2_node.append(NW2[i][node_str])
+            else:
+                NW2_node.append(0)
+        plt.plot(list(range(len(NW1))), NW1_node, color = 'b')
+        plt.plot(list(range(len(NW2))), NW2_node, color = 'r')
+        plt.title(r"$\bf{" + node + "}$")
+    lines = []
+    lines.append(mlines.Line2D([], [], color='b', label='APO'))
+    lines.append(mlines.Line2D([], [], color='r', label='PRFAR'))
+    f.legend(handles=lines, fontsize=8, loc="lower right", ncol=2, fancybox=True, shadow=True)
+    plt.tight_layout()
+    plt.savefig('/home/aghee/results/long_range_evolution/'+string+'.svg')
+    plt.close()
+
+multiplot(L_bottom, "bottom")
+multiplot(L_interface, "interface")
+multiplot(L_sb, "sb")
+multiplot(L_unfolding, "unfolding")
+multiplot(L_up, "up")
