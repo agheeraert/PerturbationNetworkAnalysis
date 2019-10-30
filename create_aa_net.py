f8316fc43d417dd53469e8ae81df9d7cdc055243
Aria GHEERAERT <aria.gheeraert@gmail.com>
2019-10-30 13:19:29 +0100

    commit of create aa net

---
+++ create_aa_net.py
@@ -0,0 +1,34 @@
+"""Adapted from Lorenza Pacini's code 
+https://github.com/lorpac
+
+Main file to compute perturbation network
+"""
+
+from CreateNetwork import CreateNetwork
+import os 
+from os.path import join as jn
+import argparse
+import numpy as np
+
+parser = argparse.ArgumentParser(description='Create the perturbation network between two proteins')
+parser.add_argument('f',  type=str,
+                    help='Input file/folder of the unperturbed state')
+parser.add_argument('o',  type=str,
+                    help='Name of the output')
+parser.add_argument('-avg',  type=str,
+                    help='Does the average of the input on folders')
+parser.add_argument('-cutoffs', type=int, nargs='+', default=[5],
+		    help='Set a list of interaction cutoffs (in Angstrom) (can be only one number)')    
+
+args = parser.parse_args()
+    
+def mkdir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+for cutoff in args.cutoffs:
+    if args.avg:
+        CreateNetwork(cutoff=cutoff).save_avg(args.f, args.o) 
+    else:
+        CreateNetwork(cutoff=cutoff).save(args.f, args.o)
+
